2f42a64ebc88470685c1e5be8239059c56987e66
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2024-07-07 13:13:10 +0100

    Add Search component

--- image_gallery/frontend/src/App.js
+++ image_gallery/frontend/src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
 
 function App() {
   return (
     <div className="App">
      <Header title="Image Gallery"/>
+     <Search />
     </div>
   );
 }
