2e92b31aa8a4f89b92a06fe604c136fc391a3389
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2024-07-07 16:43:52 +0100

    Add Controlled component Search with state

--- image_gallery/frontend/src/App.js
+++ image_gallery/frontend/src/App.js
@@ -1,13 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import { useState } from 'react'
 
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const handleSubmitSearch = (e) => {
+    e.preventDefault()
+    console.log(word)
+  }
+
   return (
     <div className="App">
      <Header title="Image Gallery"/>
-     <Search />
+     <Search word={word} setWord={setWord} handleSubmit={handleSubmitSearch}/>
     </div>
   );
 }
--- image_gallery/frontend/src/components/Search.js
+++ image_gallery/frontend/src/components/Search.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-const Search = () => {
+const Search = ({handleSubmit, word, setWord}) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search for image.." />
+                <Form.Control 
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for image.."
+                 />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
