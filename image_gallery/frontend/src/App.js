8edc45a90bc676c3b52ce943c266491861233599
Deniss B <den.b@itservicesgroup.co.uk>
2024-07-09 18:17:22 +0100

    Add fetch data from unsplash API, secrets as env vars

--- image_gallery/frontend/src/App.js
+++ image_gallery/frontend/src/App.js
@@ -3,13 +3,23 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react'
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+const API_URL = process.env.REACT_APP_API_URL
 
-function App() {
+const App = () => {
   const [word, setWord] = useState("");
 
   const handleSubmitSearch = (e) => {
     e.preventDefault()
     console.log(word)
+    fetch(`${API_URL}/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
