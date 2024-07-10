18422c55c753595f9b5cd90ebb0e72f9c66aea45
Deniss B <den.b@itservicesgroup.co.uk>
2024-07-10 18:07:20 +0100

    Add eslintr-prettier settings

--- image_gallery/frontend/src/App.js
+++ image_gallery/frontend/src/App.js
@@ -1,34 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
-const API_URL = process.env.REACT_APP_API_URL
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const App = () => {
   const [word, setWord] = useState("");
 
   const handleSubmitSearch = (e) => {
-    e.preventDefault()
-    console.log(word)
+    e.preventDefault();
+    console.log(word);
     fetch(`${API_URL}/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-      setWord("");
-  }
+        console.log(err);
+      });
+    setWord("");
+  };
 
   return (
     <div className="App">
-     <Header title="Image Gallery"/>
-     <Search word={word} setWord={setWord} handleSubmit={handleSubmitSearch}/>
+      <Header title="Image Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSubmitSearch} />
     </div>
   );
-}
+};
 
 export default App;
