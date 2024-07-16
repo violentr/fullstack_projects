a23564a2b6e832faa705085750a6ac86de9d567d
Deniss B <den.b@itservicesgroup.co.uk>
2024-07-16 18:14:08 +0100

    Add run flask app with python interpreter

--- image_gallery/api/main.py
+++ image_gallery/api/main.py
@@ -1,7 +1,9 @@
-from flask import Flask
-
+from flask import Flask # type: ignore
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def hello():
   return "Hello world!"
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5050)
