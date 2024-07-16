36e2ce6c0ea439d6cf91b8ce4f30448a5e21c2b2
Deniss B <den.b@itservicesgroup.co.uk>
2024-07-16 19:02:46 +0100

    Add retrieve data from unsplash API

--- image_gallery/api/main.py
+++ image_gallery/api/main.py
@@ -1,9 +1,24 @@
-from flask import Flask # type: ignore
+from flask import Flask, request # type: ignore
+import os
+import requests # type: ignore
+
+API_URL="https://api.unsplash.com"
+CLIENT_ID = os.getenv('UNSPLASH_ACCESS_KEY')
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello world!"
+@app.route("/new-image")
+def new_image():
+  word = request.args.get("query")
+  headers = {"Authorization": f"Client-ID {CLIENT_ID}"}
+  url = f"{API_URL}/photos/random"
+
+  r = requests.get(url=url, headers=headers, params={"query": word})
+  data = r.json()
+  return {"word": data}
+
+
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5050)
