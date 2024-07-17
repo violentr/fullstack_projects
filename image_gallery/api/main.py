6b2c4481887d42fb6e6e61a832d26ab5d45eb491
Deniss B <den.b@itservicesgroup.co.uk>
2024-07-17 17:22:04 +0100

    Add CORS, enable read data for the client

--- image_gallery/api/main.py
+++ image_gallery/api/main.py
@@ -2,16 +2,19 @@ from flask import Flask, request # type: ignore
 import os
 import requests # type: ignore
 from dotenv import load_dotenv # type: ignore
+from flask_cors import CORS # type: ignore
 
 load_dotenv(dotenv_path="./.env.local")
 # Env vars
 CLIENT_ID = os.getenv('UNSPLASH_ACCESS_KEY')
 API_URL = os.getenv('API_URL')
 DEBUG = bool(os.environ.get("DEBUG", True))
+
 if None in [CLIENT_ID, API_URL]:
   raise EnvironmentError("Please create .env.local file and provide all necessary env variables")
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
